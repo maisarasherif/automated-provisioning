4ef45ef655e4eff0bd2832221b4e5eb1bc388aba
maisarasherif <152896575+maisarasherif@users.noreply.github.com>
2024-12-26 13:08:55 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,114 @@
+provider "azurerm" {
+  subscription_id = "<your-subscription-id>"
+  client_id       = "<your-client-id>"
+  client_secret   = "<your-client-secret>"
+  tenant_id       = "<your-tenant-id>"
+
+  features {}
+}
+
+# Define resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-resources"
+  location = var.location
+}
+
+# Create the virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "devops-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "devops-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "devops-publicip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "devops-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+# Create Network Security Group and rule to allow SSH (port 22)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Attach the NSG to the NIC
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "devops-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("/path/to/your/public-key")
+  }
+
+  custom_data = base64encode(file("${path.module}/cloud-init.yaml"))
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  #edit with your preferred image
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# Output VM public IP
+output "public_ip" {
+  value = azurerm_public_ip.publicip.ip_address
+}
